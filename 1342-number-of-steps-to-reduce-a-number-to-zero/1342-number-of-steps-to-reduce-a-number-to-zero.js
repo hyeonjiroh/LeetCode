e7e0fa289230276fd9af8ec3c2b30d8296513c77
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2025-02-21 12:20:34 +0900

    Time: 1 ms (19%), Space: 52.3 MB (5.19%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function (num) {
+    let step = 0;
+
+    while (num !== 0) {
+        if (num !== 0 && num % 2 === 0) {
+            num /= 2;
+        } else if (num !== 0 && num % 2 !== 0) {
+            num -= 1;
+        }
+        step += 1;
+    }
+
+    return step;
+};
