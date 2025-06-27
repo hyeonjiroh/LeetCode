401bcf5257ff2424ccf2891fa17b720d978d7142
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2025-06-27 16:15:48 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+
+    for (let i = 0; i < flowerbed.length; i += 2) {
+        if (flowerbed[i] === 0 && !flowerbed[i - 1] && !flowerbed[i + 1]) {
+            flowerbed[i] = 1;
+            n -= 1;
+            if (n === 0) return true;
+        }
+
+    }
+
+    return n <= 0;
+};
