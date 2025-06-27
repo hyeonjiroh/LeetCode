163cc97f5d2da7e3286773fc699927957c8b6ef4
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2025-06-27 16:30:29 +0900

    Time: 1 ms (83.58%), Space: 56.5 MB (61.31%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -8,8 +8,7 @@ var canPlaceFlowers = function (flowerbed, n) {
     for (let i = 0; i < flowerbed.length; i++) {
         if (flowerbed[i] === 0 && !flowerbed[i - 1] && !flowerbed[i + 1]) {
             flowerbed[i] = 1;
-            n -= 1;
-            if (n === 0) return true;
+            n--;
         }
 
     }
