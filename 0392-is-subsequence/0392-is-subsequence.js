43a86945b46025bcc309498f4793ab0288c5aa6d
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2025-07-04 17:11:50 +0900

    Time: 0 ms (100%), Space: 55.8 MB (24.08%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,13 +4,13 @@
  * @return {boolean}
  */
 var isSubsequence = function (s, t) {
-    let i = 0; // pointer of s
+    let i = 0;
 
     for (let j = 0; j < t.length; j++) {
         if (s[i] === t[j]) {
             i++;
         }
     }
-    
+
     return i === s.length;
 };
