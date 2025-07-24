f6037774c1627920a9032e62b38e3d9f4e1e1609
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2025-07-24 09:57:23 +0900

    Time: 8 ms (81.64%), Space: 62.3 MB (35.04%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -6,9 +6,19 @@
 var findDifference = function (nums1, nums2) {
     const set1 = new Set(nums1);
     const set2 = new Set(nums2);
+    const answer = [[], []];
 
-    const onlyInNums1 = [...set1].filter(num => !set2.has(num));
-    const onlyInNums2 = [...set2].filter(num => !set1.has(num));
+    for (let i of set1) {
+        if (!set2.has(i)) {
+            answer[0].push(i);
+        }
+    }
 
-    return [onlyInNums1, onlyInNums2];
+    for (let i of set2) {
+        if (!set1.has(i)) {
+            answer[1].push(i);
+        }
+    }
+
+    return answer;
 };
