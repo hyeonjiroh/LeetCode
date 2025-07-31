0fc0f430f020e601ca9cb71768e76ea7c089dc90
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2025-07-31 15:12:25 +0900

    Time: 3 ms (53.99%), Space: 63.7 MB (7.27%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let start = 0;
+    let end = height.length - 1;
+    let waterAmount = 0;
+    let maxAmount = 0;
+
+    while (start !== end) {
+        let width = end - start;
+        let minHeight = Math.min(height[start], height[end]);
+
+        waterAmount = width * minHeight;
+        maxAmount = Math.max(waterAmount, maxAmount);
+
+        height[start] < height[end] ? start++ : end--;
+    }
+
+    return maxAmount;
+};
