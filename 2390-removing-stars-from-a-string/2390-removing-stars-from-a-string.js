9a6ba8cad7d36e772cebc5b3519877208c8a01eb
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2025-08-07 14:49:39 +0900

    Time: 34 ms (17.91%), Space: 69.7 MB (37.07%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+    const stack = [];
+
+    for (char of s) {
+        if (char === '*') {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join('');
+};
