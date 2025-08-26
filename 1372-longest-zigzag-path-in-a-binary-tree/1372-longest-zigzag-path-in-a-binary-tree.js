fb21f02ec1ba7ee1d5eaf3f013c453af35733002
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2025-08-26 18:56:42 +0900

    Time: 7 ms (88.89%), Space: 79.2 MB (34.1%) - LeetHub

---
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var longestZigZag = function (root) {
+    let maxLength = 0;
+
+    function dfs(node, isLeft, length) {
+        if (!node) return;
+
+        maxLength = Math.max(maxLength, length);
+
+        if (isLeft) {
+            dfs(node.left, false, length + 1);
+            dfs(node.right, true, 1);
+        } else {
+            dfs(node.right, true, length + 1);
+            dfs(node.left, false, 1);
+        }
+    }
+
+    dfs(root.left, false, 1);
+    dfs(root.right, true, 1);
+
+    return maxLength;
+};
+
